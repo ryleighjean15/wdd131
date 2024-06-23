ea8056bbbdf84d342212c56feb1a12306d790a91
ryleighrowell <ryleighjean15@gmail.com>
2024-06-22 21:50:12 -0700

    temple album

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('show');
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('open');
+    if (nav.classList.contains('show')) {
+        hamburger.innerHTML = '<div>X</div>';
+    } else {
+        hamburger.innerHTML = '<div></div><div></div><div></div>';
+    }
+}
+
+// Set the current year in the footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Set the last modified date in the footer
+document.getElementById('lastModified').textContent = document.lastModified;
